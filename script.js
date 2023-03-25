4003953f9eca0722e94149e4201d44055a87fa89
suffyan-zafar <66271327+suffyan-zafar@users.noreply.github.com>
2023-03-25 17:29:07 +0500

    final product

---
+++ script.js
@@ -0,0 +1,22 @@
+const text= document.querySelector('.loading-text');
+const bg=document.querySelector('.bg');
+
+let load=0;
+
+let int=setInterval(blurry,30);
+
+
+function blurry(){
+    load++;
+    if(load>99){
+        clearInterval(int);
+    }
+    console.log(load);
+    text.innerText=`${load} %`;
+    text.style.opacity=scale(load, 0,100,1,0);
+    bg.style.filter=`blur(${scale(load, 0,100,30,0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
